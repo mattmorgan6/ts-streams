9d5fcefaf3a6c377a30523c91b7fef1fa6d408f0
Matt Morgan <mattmorgan6@gmail.com>
2021-04-21 13:55:29 -0700

    Runs ixIxx

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {helloWorld} from './ixixxProcessor';
 
+
 let message: string = 'Hello World';
 //console.log(message);
 helloWorld();
--- ixixxProcessor.ts
+++ ixixxProcessor.ts
@@ -1,3 +1,18 @@
+import child = require('child_process');
+
 export function helloWorld(){
-    console.log('hello world');
+    const inputFilePath: string = "./test/test1/input.txt"
+    const ls = child.spawn('./ixIxx', [inputFilePath, "out.ix", "out.ixx"]);
+
+    ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    });
 }
