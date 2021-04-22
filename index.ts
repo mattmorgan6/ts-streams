13bfaebefefe8b6140091605e5bc79f72ff35cc7
Matt Morgan <mattmorgan6@gmail.com>
2021-04-22 14:20:11 -0700

    runIxIxx takes stream as param

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import {runIxIxx} from './ixixxProcessor';
 import {ParseGff3} from './gff3Processor';
+import fs = require('fs');
 
+/*
+// For now just read in the input file to readStream for testing.
+const inputFilePath: string = "./test/test1/input.txt";
+const readStream = fs.createReadStream(inputFilePath);
+// Test the passing in a stream to ixIxx
+runIxIxx(readStream)
+*/
 
-let message: string = 'Hello World';
-//console.log(message);
-//runIxIxx();
 ParseGff3();
--- ixixxProcessor.ts
+++ ixixxProcessor.ts
@@ -1,28 +1,14 @@
 import child = require('child_process');
 import fs = require('fs');
 
-export function runIxIxx(){
-    const inputFilePath: string = "./test/test1/input.txt";
+export function runIxIxx(readStream: fs.ReadStream){
     const ixFileName: string = "out.ix";
     const ixxFileName: string = "out.ixx";
 
-    // For now just read in the input file to readStream for testing.
-    const readStream = fs.createReadStream(inputFilePath);
-
-    // // Handle stream events --> data, end, and error
-    // readStream.on('data', function(chunk) {
-    //     console.log(data);
-    // });
-    
-    // readStream.on('end',function() {
-    //     console.log(data);
-    // });
-    
     // readStream.on('error', function(err) {
     //     console.log(err.stack);
     // });
     
-
     const ixProcess = child.spawn('cat | ./ixIxx /dev/stdin $1 $2', [ixFileName, ixxFileName], { shell: true });
     // Pass the readStream as stdin into ixProcess.
     readStream.pipe(ixProcess.stdin);
