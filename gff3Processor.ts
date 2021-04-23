3a9d8bbde35217afdf0f6e95156c43439b70aff9
Tia Flores-Carr <tia.simone01@gmail.com>
2021-04-22 17:00:34 -0700

    GFF3 file parser almost complete

--- gff3Processor.ts
+++ gff3Processor.ts
@@ -1,29 +1,46 @@
 import gff from '@gmod/gff';
 import fs = require('fs');
-import { read } from 'node:fs';
+import transformStream = require('stream');
+import { createWriteStream, read } from 'node:fs';
+import { Transform } from 'stream';
+import {runIxIxx} from './ixixxProcessor';
+
 
 export function ParseGff3(){
 
     const gff3FileName: string = "./test/AU9/single_au9_gene.gff3";
     const gff3FileName2: string = "./test/au9_scaffold_subset.gff3";
-    
+
     const gff3In = fs.createReadStream(gff3FileName2);
-    const util = gff.util;
 
+        const gffTranform = new Transform({
+            objectMode: true,
+            transform: (chunk, encoding, done) => {
+                var res;
+                chunk.forEach(record => {
+                    console.log(record.attributes.ID + " " + record.attributes.Name);
+                    res = (record.attributes.ID + " " + record.attributes.Name + '\n');
+                    
+                });
+                done(null, res);
+            }
+        })
         
-        gff3In.pipe(gff.parseStream())
-        
-        .on('data', data => {
+        let test: fs.ReadStream = gff3In.pipe(gff.parseStream({parseSequences: false})).pipe(gffTranform);
+
+        /*.on('data', data => {
             data.forEach(record => {
-                console.log(record.attributes.Name + '\t' + record.attributes.ID);
+                
+                console.log(record.attributes.ID + " " + record.attributes.Name);
             });
-            return data
         });
-        
 
+        //gff3In.pipe(gffTranform).pipe(fs.createWriteStream('out.txt'))
+    
         gff3In.on('end', () => {
             console.log('done parsing!')
-        })
-
+        });*/
+        
+        runIxIxx(test);
         
 }
