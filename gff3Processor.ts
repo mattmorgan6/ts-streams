e6cc572c4950a904c73a70835f06926627553eda
Tia Flores-Carr <tia.simone01@gmail.com>
2021-04-23 16:41:58 -0700

    Formatting and readability updates

--- gff3Processor.ts
+++ gff3Processor.ts
@@ -1,46 +1,29 @@
 import gff from '@gmod/gff';
 import fs = require('fs');
 import transformStream = require('stream');
-import { createWriteStream, read } from 'node:fs';
 import { Transform } from 'stream';
 import {runIxIxx} from './ixixxProcessor';
 
-
 export function ParseGff3(){
 
-    const gff3FileName: string = "./test/AU9/single_au9_gene.gff3";
+    const gff3FileName: string = "./test/au9_scaffold_subset_sync.gff3";
     const gff3FileName2: string = "./test/au9_scaffold_subset.gff3";
 
-    const gff3In = fs.createReadStream(gff3FileName2);
+    const gff3In = fs.createReadStream(gff3FileName);
 
-        const gffTranform = new Transform({
-            objectMode: true,
-            transform: (chunk, encoding, done) => {
-                var res;
-                chunk.forEach(record => {
-                    console.log(record.attributes.ID + " " + record.attributes.Name);
-                    res = (record.attributes.ID + " " + record.attributes.Name + '\n');
-                    
-                });
+    const gffTranform = new Transform({
+        objectMode: true,
+        transform: (chunk, encoding, done) => {
+            chunk.forEach(record => {
+                //console.log(`${record.attributes.ID} ${record.attributes.Name}\n`);
+                const res = (`${record.attributes.ID} ${record.attributes.Name}\n`);
                 done(null, res);
-            }
-        })
-        
-        let test: fs.ReadStream = gff3In.pipe(gff.parseStream({parseSequences: false})).pipe(gffTranform);
-
-        /*.on('data', data => {
-            data.forEach(record => {
-                
-                console.log(record.attributes.ID + " " + record.attributes.Name);
             });
-        });
-
-        //gff3In.pipe(gffTranform).pipe(fs.createWriteStream('out.txt'))
-    
-        gff3In.on('end', () => {
-            console.log('done parsing!')
-        });*/
+        }
+    })
         
-        runIxIxx(test);
+    const ReadFile: fs.ReadStream = gff3In.pipe(gff.parseStream({parseSequences: false})).pipe(gffTranform);
+        
+    runIxIxx(ReadFile);
         
 }
