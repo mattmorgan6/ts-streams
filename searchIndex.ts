f3dd843907a5a9f2e411f20c1aa437bf971af571
Tia Flores-Carr <tia.simone01@gmail.com>
2021-04-28 14:04:34 -0700

    Search index array

--- searchIndex.ts
+++ searchIndex.ts
@@ -7,16 +7,19 @@ import {LocalFile} from 'generic-filehandle'
 export async function doStuff(searchWord: string, ixFilePath: string, ixxFilePath: string) {
     const ixxFile = new LocalFile(ixxFilePath);
     const ixFile = new LocalFile(ixFilePath);
+    var buff;
+    let searchResult: string []= [];
 
     const trix = new Trix(ixxFile, ixFile);
 
     const results = await trix.search(searchWord);
 
-
-    results.forEach(data => {
-        const buff = Buffer.from(data, 'base64');
-        console.log(buff.toString('utf-8'));
-    });
     
-    //console.log(results);
+    results.forEach(data => {
+        buff = Buffer.from(data, 'base64');
+    });
+
+    searchResult.push(buff.toString('utf-8'));
+    console.log(`${searchResult}`);
+    
 }
