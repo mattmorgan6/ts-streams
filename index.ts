fad50f19e711824e72281de3f13f1167752943b5
Tia Flores-Carr <tia.simone01@gmail.com>
2021-04-28 13:53:36 -0700

    Trix search decoding

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {runIxIxx} from './ixixxProcessor';
 import {ParseGff3} from './gff3Processor';
 import fs = require('fs');
-import { search } from './searchIndex';
+import { doStuff } from './searchIndex';
 
 /*
 // For now just read in the input file to readStream for testing.
@@ -16,4 +16,4 @@ ParseGff3();
 const ixFileName: string = "out.ix";
 const ixxFileName: string = "out.ixx";
 
-search("au", ixFileName, ixxFileName);
+doStuff("au", ixFileName, ixxFileName);
--- searchIndex.ts
+++ searchIndex.ts
@@ -1,12 +1,22 @@
 import Trix from '@gmod/trix'
+import {LocalFile} from 'generic-filehandle'
 
 // Takes in a searchWord and searches the ix files using trix-js
 // Then returns a json object of the result.
-export function search(searchWord: string, ixFilePath: string, ixxFilePath: string){
 
-    // TODO: trix-js search
-    // npmjs.com/package/@gmod/trix
+export async function doStuff(searchWord: string, ixFilePath: string, ixxFilePath: string) {
+    const ixxFile = new LocalFile(ixxFilePath);
+    const ixFile = new LocalFile(ixFilePath);
 
-    // TODO: base-64 decode the result and return the object/s as json array.
+    const trix = new Trix(ixxFile, ixFile);
+
+    const results = await trix.search(searchWord);
+
+
+    results.forEach(data => {
+        const buff = Buffer.from(data, 'base64');
+        console.log(buff.toString('utf-8'));
+    });
     
+    //console.log(results);
 }
